3f76508d876964d208b320842329f7ec598688d1
Lazersmoke <lazersmoke@gmail.com>
2016-09-30 22:31:07 -0500

    Final (maybe) commit :D

--- src/IrvSim.hs
+++ src/IrvSim.hs
@@ -4,18 +4,31 @@ module IrvSim where
 import Data.List
 import Data.Ord
 import Data.Ratio
-import qualified Data.Set as S
 import Text.Parsec
 import Text.Parsec.String
 import Control.Monad.Writer
+import Control.Monad.State
 
-data Election = Election [Ballot] deriving Eq
+type Log a = StateT Integer (Writer String) a
+
+runLogger :: Log a -> (a,String)
+runLogger l = runWriter $ evalStateT l 0
+
+write :: String -> Log ()
+write s = do
+  i <- fromIntegral <$> get
+  tell (replicate i ' ' ++ s ++ "\n")
+
+indent :: Integer -> Log a -> Log a
+indent i l = do
+  modify (+i)
+  a <- l
+  modify (subtract i)
+  return a
+
+data Election = Election [Candidate] [Ballot] deriving Eq
 data Ballot = Ballot [Candidate] deriving Eq
-data Tally = Tally Candidate Integer deriving Eq
 newtype Candidate = Candidate String deriving Eq
-
-instance Ord Tally where
-  (Tally _ a) <= (Tally _ b) = a <= b
 
 instance Show Candidate where
   show (Candidate s) = s
@@ -25,15 +38,33 @@ instance Show Ballot where
   show (Ballot cs) = ("Ballot: "++) . intercalate ", " . map show $ cs
 
 instance Show Election where
-  show (Election []) = "No Votes in the Election"
-  show (Election bs) = intercalate "\n" . map show $ bs
+  show (Election cs []) = "Candidates were: " ++ intercalate ", " (map show cs) ++ "No votes in the election"
+  show (Election cs bs) = "Candidates were: " ++ intercalate ", " (map show cs) ++ "\n  " ++ intercalate "\n  " (map show bs)
+
+writeElection :: Election -> Log ()
+writeElection (Election [] _) = do
+  write "No Candidates Remain!"
+  write "You should check that the file parsed correctly"
+writeElection (Election cs []) = do
+  write "No votes remaining"
+  write "Remaining Candidates:"
+  write $ intercalate ", " (map show cs)
+writeElection (Election cs bs) = do
+  write "Remaining Candidates:"
+  indent 2 . write $ intercalate ", " (map show cs)
+  write "Tallies for Candidates:"
+  indent 2 $ forM_ cs fancyWrite
+  where
+    fancyWrite c = 
+      write $ show c ++ ": " ++ show (tally bs c)
+      --write $ replicate (fromIntegral $ tally bs c) '*'
 
 runElection :: String -> IO (Maybe Candidate)
-runElection fn = do
-  parseFromFile election fn >>= \case
+runElection fn =
+  parseElection fn >>= \case
     Left e -> print e *> pure Nothing
     Right e -> do
-      let (mc,l) = runWriter (resolveElection e)
+      let (mc,l) = runLogger (resolveElection e)
       putStrLn l
       pure mc
 
@@ -47,45 +78,38 @@ lexString :: String -> Parser String
 lexString = lexeme . string
 
 election :: Parser Election
-election = Election <$> ballot `endBy1` lexString "\n"
+election = Election <$> candidate `sepBy` lexString "|" <*> ballot `endBy1` lexString "\n"
 
 ballot :: Parser Ballot
 ballot = Ballot <$> candidate `sepBy` lexString ","
 
 candidate :: Parser Candidate
-candidate = Candidate <$> lexeme (many1 alphaNum)
+candidate = Candidate <$> lexeme (many1 (alphaNum <|> oneOf ['_','.']))
 
-resolveElection :: Election -> Writer String (Maybe Candidate)
+resolveElection :: Election -> Log (Maybe Candidate)
 -- No ballots, no one wins
-resolveElection (Election []) = pure Nothing
+resolveElection (Election _ []) = pure Nothing
 -- If there are ballots, then
-resolveElection e@(Election bs) = do
-  tell ("Resolving Election:\n" ++ show e ++ "\n")
-  tell ("Here are the tallies:\n" ++ (cs >>= fancyShow) ++ "\n") 
+resolveElection e@(Election cs bs) = do
+  write "Resolving Election:"
+  indent 2 $ writeElection e
   -- If anyone has more than half the votes then they win
   if percent topVoted > (1%2)
     -- Last is the highest percent
     then do
-      tell (show topVoted ++ " won because they have the highest percent!\n")
+      write (show topVoted ++ " won because they have the highest percent!")
       pure $ Just topVoted
     -- Otherwise, eliminate the lowest voted person and run a new election
-    else resolveElection =<< eliminate bottomVoted e
+    -- Write a newline to seperate elections
+    else resolveElection =<< eliminate bottomVoted e <* write ""
   where
-    fancyShow c = show c ++ " " ++ replicate (fromIntegral $ tally bs c) '*' ++ "\n"
-    cs = getCandidates bs
     total = sum (map (tally bs) cs)
     percent c = tally bs c % total
     sorted = sortBy (comparing percent) cs
     topVoted = last sorted
     bottomVoted = head sorted
 
-getCandidates :: [Ballot] -> S.Set Candidate
--- If there are any ballots at all, then include their candidates
-getCandidates ((Ballot cs):bs) = S.union (S.fromList cs) (getCandidates bs)
--- If there are no ballots, then there can be no candidates
-getCandidates [] = S.empty
-
-tally :: [Ballot] -> Candidate -> Tally
+tally :: [Ballot] -> Candidate -> Integer
 -- If there is a non-empty ballot that matches the candidate, add one
 tally (Ballot (x:_):bs) c = (if c == x then (+1) else id) $ tally bs c
 -- If there is an empty ballot, ignore it
@@ -93,10 +117,18 @@ tally (Ballot []:bs) c = tally bs c
 -- If we are out of ballots, then no votes
 tally [] _ = 0
 
-eliminate :: Candidate -> Election -> Writer String Election
-eliminate c (Election bs) = do
-  tell ("Eliminating " ++ show c ++ " from the election\n")
-  pure $ Election (map moveVote bs)
+eliminate :: Candidate -> Election -> Log Election
+eliminate c (Election cs bs) = 
+  Election
+    <$> pure (delete c cs) 
+    <*> mapM moveVote bs <* write ("Eliminating " ++ show c ++ " from the election")
   where
-    moveVote (Ballot (x:xs)) = Ballot $ if c == x then xs else x:xs
-    moveVote (Ballot []) = Ballot []
+    -- There is somewhere to move this vote to
+    moveVote (Ballot (x:xs)) = 
+      if c == x 
+        then Ballot <$> pure xs <*
+          case xs of 
+            (y:_) -> write ("Moving vote from " ++ show c ++ " to " ++ show y) 
+            [] -> write ("Vote for " ++ show c ++ " has no further candidate to move to, and is eliminated.") 
+        else Ballot <$> pure (x:xs)
+    moveVote (Ballot []) = pure $ Ballot []
